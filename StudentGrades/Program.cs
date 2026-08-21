275bcc453b9e2ca9a6fd96258bcf7563d310708c
Marina <ninarodrgs@gmail.com>
2026-08-21 09:43:19 -0300

    Organizando pastas e adicionando projeto StudentGrades

---
+++ StudentGrades/Program.cs
@@ -0,0 +1,116 @@
+﻿int examAssignments = 5;
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+
+int[] studentScores = new int[10];
+
+string currentStudentLetterGrade = "";
+
+// display the header row for scores/grades
+
+Console.Clear();
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    decimal sumAssignmentScores = 0;
+    decimal currentStudentGrade = 0;
+    int gradedAssignments = 0;
+    int extraCreditAssignments = 0;
+    decimal gradedScore = 0;
+    int examScore = 0;
+    int extraCreditScore = 0;
+    int extraCreditTotal = 0;
+    decimal medianScore = 0;
+
+    foreach (int score in studentScores)
+    {
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+        {
+            sumAssignmentScores += score;
+            examScore += score;
+        }
+        else
+        {
+          //sumAssignmentScores += score / 10;     
+          sumAssignmentScores += (decimal)score / 10m;
+            extraCreditAssignments += 1;
+        }
+        if (gradedAssignments >= 6)
+        {
+            extraCreditTotal += score;
+        }
+
+    }
+
+    extraCreditScore = extraCreditTotal / extraCreditAssignments;
+    gradedScore = (decimal)(examScore) / examAssignments;
+    currentStudentGrade = (decimal)(sumAssignmentScores) / (decimal)(examAssignments);
+    medianScore = (decimal)(currentStudentGrade) - (decimal)(gradedScore);
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{gradedScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t {extraCreditScore} ({medianScore} pts)");
+
+}
+
+Console.WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
